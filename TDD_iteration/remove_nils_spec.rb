226132db9c75770ca61ce2563f1d46730a101585
oliverswitzer <oliverswitzer@gmail.com>
2013-11-19 19:56:40 -0500

    add another test to make sure remove_nils some_method does not raise an error

--- TDD_iteration/remove_nils_spec.rb
+++ TDD_iteration/remove_nils_spec.rb
@@ -3,6 +3,10 @@ require './remove_nils_solution'
 
 describe "#some_method" do
   
+  it "should not return an error" do
+    expect(some_method).to_not raise_error
+  end
+  
   it "should return an the sum of an array 
   that has nils (hardcoded as [1, 4, nil, 9, 16, nil])" do
     expect(some_method).to eq(30)
